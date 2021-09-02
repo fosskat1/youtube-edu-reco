b15ced9eac0fcc2ffe72f98677d01f09d984185c
Katie Foss <katiefos@usc.edu>
2021-09-01 17:10:16 -0700

    get noun phrase from nlp

--- natural_language_processing/topic.py
+++ natural_language_processing/topic.py
@@ -4,9 +4,14 @@ test_sentences = ["I want to learn physics",
                   "Teach me physics.",
                   "Help! I don't understand physics."]
 
-sentence = test_sentences[0]
-tokens = nltk.word_tokenize(sentence)
-tagged = nltk.pos_tag(tokens)
-entities = nltk.chunk.ne_chunk(tagged)
 
-print(entities)
+def topic(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+    grammar = "NP: {<JJ>*<NN>*<NNS>}"
+    cp = nltk.RegexpParser(grammar)
+    return cp.parse(tagged)
+
+
+topic(test_sentences[0]).pprint()
+# topic(test_sentences[0]).draw()
