ac292f711d802969ff235dbca0f452a1de492a88
Soofie Motamedi <soofiemotamedi20@berkeley.edu>
2021-09-15 00:12:25 -0700

    max results set to 20 for now

--- return_search_results.py
+++ return_search_results.py
@@ -4,11 +4,11 @@ from googleapiclient.discovery import build
 def return_search_results(search_term):
     API_VERSION = 'v3'
     SERVICENAME = 'youtube'
-    DEVELOPER_KEY = 'AIzaSyChnpuiesF1TN1aLmylKumzL6ahB7F6WxY'
+    DEVELOPER_KEY = 'AIzaSyBJiIF6-PXhzyfLVJRBseLGZXvc34vPbPY'
     with build(serviceName=SERVICENAME, version=API_VERSION,
                developerKey=DEVELOPER_KEY) as service:
         request = service.search().list(q=search_term, part='snippet',
-                                        type='video', maxResults=1)
+                                        type='video', maxResults=20)
     result = request.execute()
     return result
 
